58099751eb694ceff6ec7de9d6535ba9be486166
@coder shruti <www.meshruti@gmail.com>
2023-12-28 01:18:37 +0530

    add valid product

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -12,23 +12,23 @@ const Nav=()=>{
     }
     return(
         <div>
-            <ul className="nav-ul">
+            {
+                  auth ? <ul className="nav-ul">
                 <li><Link to="/">Products</Link></li>
                 <li><Link to="/add">Add Product</Link></li>
                 <li><Link to="/update">Update Product</Link></li>
                 <li><Link to="/profile">Profile</Link></li>
-                {
-                  auth ?<li><Link onClick={logout} to="/signup">Logout</Link></li>
-                  : <>
+                <li><Link onClick={logout} to="/signup">Logout ({JSON.parse(auth).name})</Link></li>
+                </ul>
+                :
+                <ul className="nav-ul nav-right">
                 <li><Link to="/signup">Sign Up</Link></li>
                 <li><Link to="/login">Login</Link></li> 
                   
-                  </>
+                </ul>
 
 
                 }
-
-            </ul>
         </div>
     )
 }
